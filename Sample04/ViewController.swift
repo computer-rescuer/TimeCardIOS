9ea1204ca350533e99b17e73bc3d8dc1837d6098
ishimotoren <r-ishimoto@computer-rescuer.com>
2020-10-20 08:31:46 +0900

    10/20 8:22

--- Sample04/ViewController.swift
+++ Sample04/ViewController.swift
@@ -3,7 +3,7 @@
 //  Sample04
 //
 //  Created by Computer=Rescuer.com on 2020/10/17.
-//git
+//
 
 import UIKit
 
@@ -14,22 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var L2: UILabel!
     @IBOutlet weak var T1: UITextView!
     
+    @IBOutlet weak var UserID: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var Name: UITextField!
+    @IBOutlet weak var Syain_cd: UITextField!
+    @IBOutlet weak var Area1: UITextField!
+    @IBOutlet weak var Area2: UITextField!
+    @IBOutlet weak var Area3: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         crk_upload();
        // let crk_lable_ins = crk_label();
-        L2.text="2"
-        L1.text = "Befor2"
+//        L2.text="2"
+//        L1.text = "Befor"
         Download_crk(stUrl: "https://minkara.carview.co.jp",
             fn: { data in
                 DispatchQueue.main.async {
                     //取得した文字列データをUITextViewに収納
-                    self.T1.text = data
+//                    self.T1.text = data
                 }
         })
 
-        
+
     }
     @IBAction func Push(_ sender: Any) {
         L1.text = "yyy"
@@ -50,10 +58,31 @@ class ViewController: UIViewController {
         }
     }
 
-    
+    @IBAction func Save(_ sender: Any) {
+     
+        let tf1  = UserID.text!
+        let tf2  = Password.text!
+        let tf3  = Name.text!
+        let tf4  = Name.text!
+        let tf5  = Name.text!
+        let tf6  = Name.text!
+        let tf7  = Name.text!
+        
+        
+        let alltf = tf1 + "," + tf2 + "," + tf3
+        /// ①DocumentsフォルダURL取得
+        guard let dirURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            fatalError("フォルダURL取得エラー")
+        }
+        
+        /// ②対象のファイルURL取得
+        let fileURL = dirURL.appendingPathComponent("output1.txt")
 
+        /// ③ファイルの書き込み
+        do {
+            try alltf.write(to: fileURL, atomically: true, encoding: .utf8)
+        } catch {
+            print("Error: \(error)")
+        }
+    }
 }
-    
-
-
-
---
+++ Sample04/test1/test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by CRK on 2020/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
